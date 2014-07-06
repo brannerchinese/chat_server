ff8ee764dae2a42d435424caebc0aad6c8a3e0a8
dpb15 <brannerchinese@gmail.com>
2014-07-06 18:29:32 -0400

    add forward_reverse.py converted to Py3K

--- echo_server/rhodes_goerzen_foundations/www_ping.py
+++ echo_server/rhodes_goerzen_foundations/www_ping.py
@@ -7,24 +7,24 @@
 import socket, sys
 
 if len(sys.argv) != 2:
-	sys.stderr.write('usage: www_ping.py <hostname_or_ip>')
-	sys.exit(2)
+    sys.stderr.write('usage: www_ping.py <hostname_or_ip>')
+    sys.exit(2)
 hostname_or_ip = sys.argv[1]
 try:
-	infolist = socket.getaddrinfo(
-		hostname_or_ip, 'www', 0, socket.SOCK_STREAM, 0,
-		socket.AI_ADDRCONFIG | socket.AI_V4MAPPED | socket.AI_CANONNAME,
-		)
+    infolist = socket.getaddrinfo(
+        hostname_or_ip, 'www', 0, socket.SOCK_STREAM, 0,
+        socket.AI_ADDRCONFIG | socket.AI_V4MAPPED | socket.AI_CANONNAME,
+        )
 except socket.gaierror as e:
-	print('Name service failure: {}'.format(e.args[1]))
-	sys.exit(1)
+    print('Name service failure: {}'.format(e.args[1]))
+    sys.exit(1)
 info = infolist[0] # per standard recommendation, try the first one
 socket_args = info[0:3]
 address = info[4]
 s = socket.socket(*socket_args)
 try:
-	s.connect(address)
+    s.connect(address)
 except socket.error as e:
-	print('Network failure: {}'.format(e.args[1]))
+    print('Network failure: {}'.format(e.args[1]))
 else:
-	print('Success: host {} is listening on port 80'.format(info[3]))
+    print('Success: host {} is listening on port 80'.format(info[3]))
